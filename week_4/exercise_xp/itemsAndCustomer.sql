f00db6114df1e99a148414aba4ec8ea941a39e1d
Nomekuma <t.guness35@gmail.com>
2024-05-06 13:12:56 +0400

    chore: update .gitignore to exclude .vscode/

---
+++ week_4/exercise_xp/itemsAndCustomer.sql
@@ -0,0 +1,6 @@
+SELECT * FROM items;
+SELECT * FROM items WHERE price > 80;
+SELECT * FROM items WHERE price < 300;
+SELECT * FROM customer WHERE last_name = 'Smith';
+SELECT * FROM customer WHERE first_name = 'John';
+SELECT * FROM customer WHERE last_name != 'Smith';
