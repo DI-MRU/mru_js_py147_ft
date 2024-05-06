a4ebfcb5e0aea501df6106bba4649b2bdd5886e1
Nomekuma <t.guness35@gmail.com>
2024-05-06 13:13:32 +0400

    chore: update customer query to exclude last name 'Scott'

--- week_4/exercise_xp/itemsAndCustomer.sql
+++ week_4/exercise_xp/itemsAndCustomer.sql
@@ -3,4 +3,4 @@ SELECT * FROM items WHERE price > 80;
 SELECT * FROM items WHERE price < 300;
 SELECT * FROM customer WHERE last_name = 'Smith';
 SELECT * FROM customer WHERE first_name = 'John';
-SELECT * FROM customer WHERE last_name != 'Smith';
+SELECT * FROM customer WHERE last_name != 'Scott';
