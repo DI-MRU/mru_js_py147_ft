690ef6d43cb866decfa14685f3d89e5499b5f79b
Abdallah Ramsing <yashircr@gmail.com>
2024-05-09 11:52:28 +0400

    Adding daily challenge -- basic intro

---
+++ week4/daily_challenge/codes.py
@@ -0,0 +1,27 @@
+
+# Using this REST Countries API, create the functionality which will write 10 random countries to your database.
+# These are the attributes which you should populate your tables with: name, capital, flag, subregion, population.
+
+# 1. create a table called countries with name, capital, flag, subregion, population
+# name -> varchar
+# capital -> varchar
+# population -> number / integer
+
+
+# 3 ways to create your database
+# 1. manually on GUI pgadmin
+# 2. create it using CLI / pgsql
+# 3. create it using codes <--
+
+# codes to connect to the postgres database and database
+# you can also use a function to connect to the database
+
+def get_countries():
+    pass
+
+def create_table():
+    # follow codes in the lessons for example
+    pass
+
+def insert_countries():
+    pass
