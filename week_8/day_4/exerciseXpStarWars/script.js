312779a3b4ae7e2443a637e98b2c5ab88a3ae5f4
Nomekuma <t.guness35@gmail.com>
2024-06-05 12:26:43 +0400

    feat: starWars DailyChallenge

---
+++ week_8/day_4/exerciseXpStarWars/script.js
@@ -0,0 +1,29 @@
+const container = document.querySelector(".container");
+const findsomeone = document.querySelector("#findsomeone");
+const loading = document.querySelector("#loading");
+
+loading.style.display = "none";
+
+function getPerson() {
+  // Show the loading spinner
+  loading.style.display = ""; // Display the loading spinner
+
+  const random = Math.floor(Math.random() * 82);
+  fetch(`https://www.swapi.tech/api/people/${random}`)
+    .then((response) => response.json())
+    .then((data) => {
+      const person = data.result.properties;
+      container.innerHTML = `
+            <h2>${person.name}</h2>
+            <p>Height: ${person.height}</p>
+            <p>Gender: ${person.gender}</p>
+            <p>Birth Year: ${person.birth_year}</p>
+            <p>Homeworld: ${person.homeworld}</p>
+            `;
+
+      // Hide the loading spinner
+      loading.style.display = "none";
+    });
+}
+
+findsomeone.addEventListener("click", getPerson);
