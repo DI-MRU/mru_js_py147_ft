64faad61afcea5c08131fba387ec56361389b344
Nomekuma <t.guness35@gmail.com>
2024-06-21 12:42:29 +0400

    feat: Improve error handling in actor routes

--- week_10/day_5/recap/index.js
+++ week_10/day_5/recap/index.js
@@ -19,18 +19,29 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   // SELECT * FROM actors
-  db.select("*")
-    .from("actors")
-    .then((actors) => res.send(actors));
+  try {
+    db.select("*")
+      .from("actors")
+      .then((actors) => res.send(actors))
+      .catch((err) => res.send(err.message));
+  } catch (err) {
+    res.send(err.message);
+  }
 });
 
 app.get("/:actor_id", (req, res) => {
   const { actor_id } = req.params;
-  // SELECT * from actors WHERE actor_id={anyIdNumberThatExists} e.g. 1
-  db.select("*")
-    .from("actors")
-    .where({ actor_id })
-    .then((actor) => res.send(actor));
+
+  try {
+    // SELECT * from actors WHERE actor_id={anyIdNumberThatExists} e.g. 1
+    db.select("*")
+      .from("actors")
+      .where({ actor_id })
+      .then((actor) => res.send(actor))
+      .catch((err) => res.send(err.message));
+  } catch (err) {
+    res.send(err.message);
+  }
 });
 
 // INSERT INTO actors(first_name,last_name,birthdate,number_oscars,is_active) VALUES('bob','paul','2024-01-01', 100 ,true);
@@ -38,16 +49,20 @@ app.get("/:actor_id", (req, res) => {
 app.post("/", (req, res) => {
   const { first_name, last_name, birthdate, number_oscars, is_active } =
     req.body;
-  db.insert({
-    first_name: first_name,
-    last_name: last_name,
-    birthdate: birthdate,
-    number_oscars: number_oscars,
-    is_active: is_active,
-  })
-    .from("actors")
-    .then(() => res.send({ message: "actors was saved successfully" }))
-    .catch((err) => res.send({ err: err, message: "There was an error" }));
+  try {
+    db.insert({
+      first_name: first_name,
+      last_name: last_name,
+      birthdate: birthdate,
+      number_oscars: number_oscars,
+      is_active: is_active,
+    })
+      .from("actors")
+      .then(() => res.send({ message: "actors was saved successfully" }))
+      .catch((err) => res.send(err.message));
+  } catch (err) {
+    res.send(err.message);
+  }
 });
 
 // update actors set first_name = 'Bob' where actor_id = 11
@@ -55,26 +70,35 @@ app.put("/:actor_id", (req, res) => {
   const { actor_id } = req.params;
   const { first_name, last_name, birthdate, number_oscars, is_active } =
     req.body;
-
-  db.update({
-    first_name: first_name,
-    last_name: last_name,
-    birthdate: birthdate,
-    number_oscars: number_oscars,
-    is_active: is_active,
-  })
-    .from("actors")
-    .where({ actor_id })
-    .then(() => res.send({ message: "actors was updated successfully" }));
+  try {
+    db.update({
+      first_name: first_name,
+      last_name: last_name,
+      birthdate: birthdate,
+      number_oscars: number_oscars,
+      is_active: is_active,
+    })
+      .from("actors")
+      .where({ actor_id })
+      .then(() => res.send({ message: "actors was updated successfully" }))
+      .catch((err) => res.send(err.message));
+  } catch (err) {
+    res.send(err.message);
+  }
 });
 
 // delete from actors where actor_id= 1
 app.delete("/:actor_id", (req, res) => {
   const { actor_id } = req.params;
-  db.delete()
-    .from("actors")
-    .where({ actor_id })
-    .then(() => res.send({ message: "actor was deleted successfully" }));
+  try {
+    db.delete()
+      .from("actors")
+      .where({ actor_id })
+      .then(() => res.send({ message: "actor was deleted successfully" }))
+      .catch((err) => res.send(err.message));
+  } catch (err) {
+    res.send(err.message);
+  }
 });
 
 app.listen(3000, () => {
