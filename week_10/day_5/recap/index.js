bc0ab10a8d0a938669474d7cb1ae66d38559f317
Nomekuma <t.guness35@gmail.com>
2024-06-21 12:08:19 +0400

    update: week 10 day 5 recap KNEX

--- week_10/day_5/recap/index.js
+++ week_10/day_5/recap/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-
+// CRUD
 const db = require("knex")({
   client: "pg",
   connection: {
@@ -33,8 +33,7 @@ app.get("/:actor_id", (req, res) => {
     .then((actor) => res.send(actor));
 });
 
-// INSERT INTO actors(first_name,last_name,birthdate,number_oscars,is_active)
-//  VALUES('bob','paul','2024-01-01', 100 ,true);
+// INSERT INTO actors(first_name,last_name,birthdate,number_oscars,is_active) VALUES('bob','paul','2024-01-01', 100 ,true);
 
 app.post("/", (req, res) => {
   const { first_name, last_name, birthdate, number_oscars, is_active } =
@@ -49,6 +48,33 @@ app.post("/", (req, res) => {
     .from("actors")
     .then(() => res.send({ message: "actors was saved successfully" }))
     .catch((err) => res.send({ err: err, message: "There was an error" }));
+});
+
+// update actors set first_name = 'Bob' where actor_id = 11
+app.put("/:actor_id", (req, res) => {
+  const { actor_id } = req.params;
+  const { first_name, last_name, birthdate, number_oscars, is_active } =
+    req.body;
+
+  db.update({
+    first_name: first_name,
+    last_name: last_name,
+    birthdate: birthdate,
+    number_oscars: number_oscars,
+    is_active: is_active,
+  })
+    .from("actors")
+    .where({ actor_id })
+    .then(() => res.send({ message: "actors was updated successfully" }));
+});
+
+// delete from actors where actor_id= 1
+app.delete("/:actor_id", (req, res) => {
+  const { actor_id } = req.params;
+  db.delete()
+    .from("actors")
+    .where({ actor_id })
+    .then(() => res.send({ message: "actor was deleted successfully" }));
 });
 
 app.listen(3000, () => {
